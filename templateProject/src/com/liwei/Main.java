f96fe80396cdba6f0550b7df5ac88c02d2714303
liwei-swust <liwei_swust@163.com>
2019-11-12 14:07:31 +0800

    提交模板以及初始ignore
    提交LeetCode项目模板以及初始ignore

---
+++ templateProject/src/com/liwei/Main.java
@@ -0,0 +1,11 @@
+package com.liwei;
+/**
+ * 
+ * @author liwei
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ templateProject/src/com/liwei/Solution.java
@@ -0,0 +1,5 @@
+package com.liwei;
+
+class Solution {
+  
+}
