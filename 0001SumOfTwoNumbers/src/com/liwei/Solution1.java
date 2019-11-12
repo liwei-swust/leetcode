12039e2b00cde0db21ad8f0be37fb6ad6df1594f
liwei-swust <liwei_swust@163.com>
2019-11-12 18:20:55 +0800

    修改代码到更加标准
    修改代码到更加标准

--- 0001SumOfTwoNumbers/src/com/liwei/Solution1.java
+++ 0001SumOfTwoNumbers/src/com/liwei/Solution1.java
@@ -11,10 +11,7 @@ public class Solution1 extends AbstractSolution{
 	public int[] twoSum(int[] nums, int target) {
 		int [] result = new int[2];
 		for(int i = 0 ; i<nums.length ; i++){
-			for(int j = 0 ; j<nums.length ; j++){
-				if(i == j){
-					continue;
-				}
+			for(int j = i+1 ; j<nums.length ; j++){
 				if(target == nums[i] + nums[j]){
 					result[0] = i;
 					result[1] = j;
